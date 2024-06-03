70c1510dd5fa463aab8ba766b319dfc5aa784fec
ebenezeraladesuyi <ebenezeraladesuyi@gmail.com>
2024-06-03 15:39:44 +0100

    contact

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -4,6 +4,8 @@ import { lazy } from "react";
 import HomeLayout from "../layout/HomeLayout";
 import MemberLayout from "../layout/MemberLayout";
 import AboutLayout from "../layout/AboutLayout";
+import ContactLayout from "../layout/ContactLayout";
+// import Contact from "../pages/contact/Contact";
 // import AboutComp from "../pages/about/AboutComp";
 // import Member from "../pages/Member/Member";
 // import HomeComp from "../pages/homepage/HomeComp";
@@ -12,6 +14,7 @@ import AboutLayout from "../layout/AboutLayout";
 const HomeComp = lazy(() => import("../pages/homepage/HomeComp"));
 const Member = lazy(() => import("../pages/Member/Member"));
 const AboutComp = lazy(() => import("../pages/about/AboutComp"));
+const Contact = lazy(() => import("../pages/contact/Contact"));
 
 
 export const element = createBrowserRouter([
@@ -42,6 +45,16 @@ export const element = createBrowserRouter([
             {
                 index: true,
                 element: <AboutComp />
+            }
+        ]
+    },
+    {
+        path:"/contact",
+        element: <ContactLayout />,
+        children: [
+            {
+                index: true,
+                element: <Contact />
             }
         ]
     },

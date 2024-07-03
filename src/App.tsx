1d9e3ecab834623fe5fa5d3d37bfc54b6edd495b
ebenezeraladesuyi <ebenezeraladesuyi@gmail.com>
2024-07-03 06:57:19 +0100

    pay

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
 // src/App.tsx
 
 import React from 'react';
-import { Elements } from '@stripe/react-stripe-js';
-import { StripeProvider, useStripeContext } from './pages/payment/StripeContext';
-import PaymentForm from './pages/payment/PaymentForm';
-// import { StripeProvider, useStripeContext } from './StripeContext';
-// import PaymentForm from './PaymentForm';
 
 const App: React.FC = () => {
   return (
-    <StripeProvider>
-      <div className="App">
-        <Elements stripe={useStripeContext().stripe!}>
-          <PaymentForm />
-        </Elements>
-      </div>
-    </StripeProvider>
+    <>
+    </>
   );
 };
 
